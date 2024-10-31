7f5dc9a3cb4fcec2c3c7cdc626e775c81ab223a8
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-31 23:05:20 +0700

    feat: update

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
   project = "suara-nusa-labs"
-  region  = "us-west1" // Jakarta
+  region  = var.region// Jakarta
 }
 
 provider "github" {
@@ -8,11 +8,15 @@ provider "github" {
   owner = var.github_username
 }
 
-module "cloud_build" {
-  source          = "./modules/cloud-build"
-  github_username = var.github_username
-  project_id      = var.project_id
-  region          = var.region
-  repository_name = var.repository_name
+# module "cloud_build" {
+#   source          = "./modules/cloud-build"
+#   github_username = var.github_username
+#   project_id      = var.project_id
+#   region          = var.region
+#   repository_name = var.repository_name
+# }
+
+module "service_account" {
+  source = "./modules/service-account"
 }
 
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "suara-nusa-labs"
+region = "us-west1"
