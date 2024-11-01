6785a58a22be7ffe68710ece33b4dad533405102
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-02 00:13:44 +0700

    feat: output image name

--- modules/cloud-build/main.tf
+++ modules/cloud-build/main.tf
@@ -23,3 +23,13 @@ resource "google_cloudbuild_trigger" "trigger-api" {
     images = ["gcr.io/${var.project_id}/suara-nusa-api"]
   }
 }
+
+# Menggunakan data source untuk mengambil informasi image
+data "google_container_registry_image" "suara_nusa_api_image" {
+  name = "gcr.io/${var.project_id}/suara-nusa-api"
+}
+
+
+output "suara_nusa_api_image_name" {
+  value = data.google_container_registry_image.suara_nusa_api_image.name
+}
