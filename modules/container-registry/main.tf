e3383e341aba5995fe463da17f3c88576325cb8f
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-02 14:53:57 +0700

    feat: container registry

--- modules/container-registry/main.tf
+++ modules/container-registry/main.tf
@@ -1,5 +1,5 @@
 resource "google_container_registry" "registry" {
   project  = var.project_name
-  location = var.location
+  location = "US"
 }
 
--- modules/container-registry/variables.tf
+++ modules/container-registry/variables.tf
@@ -2,8 +2,3 @@ variable project_name {
   description = "Name of the Github service project"
   type        = string
 }
-
-variable location {
-  description = "Location of the registry"
-  type        = string
-}
