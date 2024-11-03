484c496fd1a484300c058ef871afedec63839437
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-03 21:40:20 +0700

    feat: init sh

--- initial.sh
+++ initial.sh
@@ -5,4 +5,6 @@ for SERVICE in $(gcloud services list --enabled --project suara-nusa-labs --form
 done
 
 # Make repo in build
- gcloud builds connections create github suara-nusa-repository --region=us-west1
+ gcloud builds connections create github suara-nusa-repository --region=us-central1
+
+ gcloud projects get-iam-policy suara-nusa-labs --format=json | jq -r '.bindings[] | select(.members[] | contains("user:serviceAccount:service-158751523704@gcp-sa-cloudbuild.iam.gserviceaccount.com")) | .role'
