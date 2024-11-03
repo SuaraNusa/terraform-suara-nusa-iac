c8170e49ee5ce3fb28978636793f14c3eca5107f
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-03 21:41:12 +0700

    feat: change location

--- modules/container-registry/main.tf
+++ modules/container-registry/main.tf
@@ -1,5 +1,5 @@
 resource "google_artifact_registry_repository" "suara-nusa-repository" {
-  location      = "us-west1"
+  location      = "us-central1"
   repository_id = var.project_id
   description   = "Docker repository for using the schema registry"
   format        = "DOCKER"
