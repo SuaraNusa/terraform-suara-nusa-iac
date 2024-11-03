b0a41e51f05b9de7558346bd17623f88ea043cbd
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-03 21:41:58 +0700

    feat: main

--- main.tf
+++ main.tf
@@ -3,27 +3,27 @@ provider "google" {
   region  = var.region
 }
 
-#
-# module "cloud_build" {
-#   source             = "./modules/cloud-build"
-#   project_id         = var.project_id
-#   region             = var.region
-#   project_number     = var.project_number
-#   service_account_id = module.service_account.cloudbuild_service_account_id
-# }
-#
-# module "container_registry" {
-#   source     = "./modules/container-registry"
-#   project_id = var.project_id
-#
-# }
-#
-#
-# module "service_account" {
-#   source               = "./modules/service-account"
-#   service_account_role = var.service_account_role
-#   project_id           = var.project_id
-# }
+
+module "cloud_build" {
+  source             = "./modules/cloud-build"
+  project_id         = var.project_id
+  region             = var.region
+  project_number     = var.project_number
+  service_account_id = module.service_account.cloudbuild_service_account_id
+}
+
+module "container_registry" {
+  source     = "./modules/container-registry"
+  project_id = var.project_id
+
+}
+
+
+module "service_account" {
+  source               = "./modules/service-account"
+  service_account_role = var.service_account_role
+  project_id           = var.project_id
+}
 
 
 module "cloud_run" {
@@ -31,10 +31,9 @@ module "cloud_run" {
   cloud_run_service_name   = "crun"
   image_name               = module.cloud_build.suara_nusa_api_image_name
   location                 = var.location
-  database_connection_name = module.cloud_sql.instance_connection_name
 }
 
 
-module "cloud_sql" {
-  source = "./modules/cloud-sql"
-}
+# module "cloud_sql" {
+#   source = "./modules/cloud-sql"
+# }
