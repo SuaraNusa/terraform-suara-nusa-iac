c761f121e1919c1496d20f02339162d48d3c1351
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-03 22:06:49 +0700

    feat: change script

--- initial.sh
+++ initial.sh
@@ -1,10 +1,10 @@
 # ONLY ENABLE REQUIRED APIs
 gcloud services enable cloudbuild.googleapis.com
-for SERVICE in $(gcloud services list --enabled --project suara-nusa-labs --format="value(config.name)"); do
-  gcloud services disable $SERVICE --project suara-nusa-labs
+for SERVICE in $(gcloud services list --enabled --project suara-nusa-dev-labs --format="value(config.name)"); do
+  gcloud services disable $SERVICE --project suara-nusa-dev-labs
 done
 
 # Make repo in build
  gcloud builds connections create github suara-nusa-repository --region=us-central1
 
- gcloud projects get-iam-policy suara-nusa-labs --format=json | jq -r '.bindings[] | select(.members[] | contains("user:serviceAccount:service-158751523704@gcp-sa-cloudbuild.iam.gserviceaccount.com")) | .role'
+ gcloud projects get-iam-policy suara-nusa-dev-labs --format=json | jq -r '.bindings[] | select(.members[] | contains("user:serviceAccount:service-158751523704@gcp-sa-cloudbuild.iam.gserviceaccount.com")) | .role'
