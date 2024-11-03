57db99986e2294e8c8ad799e584a8544ec3d3113
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-04 00:41:01 +0700

    feat: secret manager

--- modules/secret-manager/main.tf
+++ modules/secret-manager/main.tf
@@ -40,3 +40,27 @@ resource "google_cloudbuildv2_connection" "my_connection" {
   }
   depends_on = [google_secret_manager_secret_iam_policy.policy]
 }
+
+resource "google_secret_manager_secret" "database_url" {
+  secret_id = "DATABASE_URL"
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "database_url_version" {
+  secret      = google_secret_manager_secret.database_url.id
+  secret_data = "mysql://root:root@localhost:3306/nest_suara_nusa_api?schema=public"
+}
+
+resource "google_secret_manager_secret" "jwt_secret_key" {
+  secret_id = "JWT_SECRET_KEY"
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "jwt_secret_key_version" {
+  secret      = google_secret_manager_secret.jwt_secret_key.id
+  secret_data = "reallySecretKey"
+}
