3c1df728f9bd7da3066ff4b09294da2c93debb6e
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-04 12:18:48 +0700

    feat: secret manager

--- modules/secret-manager/main.tf
+++ modules/secret-manager/main.tf
@@ -1,46 +1,3 @@
-// Create a secret containing the personal access token and grant permissions to the Service Agent
-resource "google_secret_manager_secret" "github_token_secret" {
-  project   = PROJECT_ID
-  secret_id = SECRET_ID
-
-  replication {
-    auto {}
-  }
-}
-
-resource "google_secret_manager_secret_version" "github_token_secret_version" {
-  secret      = google_secret_manager_secret.github_token_secret.id
-  secret_data = GITHUB_PAT
-}
-
-data "google_iam_policy" "serviceagent_secretAccessor" {
-  binding {
-    role = "roles/secretmanager.secretAccessor"
-    members = ["serviceAccount:service-PROJECT_NUMBER@gcp-sa-cloudbuild.iam.gserviceaccount.com"]
-  }
-}
-
-resource "google_secret_manager_secret_iam_policy" "policy" {
-  project     = google_secret_manager_secret.github_token_secret.project
-  secret_id   = google_secret_manager_secret.github_token_secret.secret_id
-  policy_data = data.google_iam_policy.serviceagent_secretAccessor.policy_data
-}
-
-// Create the GitHub connection
-resource "google_cloudbuildv2_connection" "my_connection" {
-  project  = PROJECT_ID
-  location = REGION
-  name     = CONNECTION_NAME
-
-  github_config {
-    app_installation_id = INSTALLATION_ID
-    authorizer_credential {
-      oauth_token_secret_version = google_secret_manager_secret_version.github_token_secret_version.id
-    }
-  }
-  depends_on = [google_secret_manager_secret_iam_policy.policy]
-}
-
 resource "google_secret_manager_secret" "database_url" {
   secret_id = "DATABASE_URL"
   replication {
@@ -63,4 +20,12 @@ resource "google_secret_manager_secret" "jwt_secret_key" {
 resource "google_secret_manager_secret_version" "jwt_secret_key_version" {
   secret      = google_secret_manager_secret.jwt_secret_key.id
   secret_data = "reallySecretKey"
+}
+
+output "jwt_secret_key_id" {
+  value = google_secret_manager_secret.jwt_secret_key.id
+}
+
+output "database_url_id" {
+  value = google_secret_manager_secret.database_url.id
 }
