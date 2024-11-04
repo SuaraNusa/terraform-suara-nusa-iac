efa7b01cf4e576f32054e12712d4344224fc700b
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-04 12:19:01 +0700

    feat: import module

--- main.tf
+++ main.tf
@@ -4,18 +4,18 @@ provider "google" {
 }
 
 module "cloud_build" {
-  source             = "./modules/cloud-build"
-  project_id         = var.project_id
-  region             = var.region
-  project_number     = var.project_number
-  service_account_id = module.service_account.cloudbuild_service_account_id
+  source                       = "./modules/cloud-build"
+  project_id                   = var.project_id
+  region                       = var.region
+  project_number               = var.project_number
+  service_account_id           = module.service_account.cloudbuild_service_account_id
   github_personal_access_token = var.github_personal_access_token
 }
 
 module "local-exec" {
-  source = "./modules/local-exec"
+  source       = "./modules/local-exec"
   trigger_name = module.cloud_build.cloudbuild_trigger_name
-  first_time = var.first_time
+  first_time   = var.first_time
 }
 
 
@@ -31,3 +31,21 @@ module "service_account" {
   project_id           = var.project_id
 }
 
+module "cloud_sql" {
+  source     = "./modules/cloud-sql"
+  project_id = var.project_id
+}
+
+module "secret_manager" {
+  source = "./modules/secret-manager"
+}
+
+module "cloud_run" {
+  source                   = "./modules/cloud-run"
+  database_connection_name = module.cloud_sql.instance_connection_name
+  image_name               = module.cloud_build.suara_nusa_api_image_name
+  region                   = var.region
+  jwt_secret_key_id        = module.secret_manager.jwt_secret_key_id
+  database_url_id          = module.secret_manager.database_url_id
+  service_account_name     = module.service_account.cloudbuild_service_account_email
+}
