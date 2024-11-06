52781c6d09cc1ecb189ac4119c8efa4778950b3c
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-07 00:56:44 +0700

    feat: cloud storage for etl job

--- modules/cloud-storage-job/main.tf
+++ modules/cloud-storage-job/main.tf
@@ -1,6 +1,6 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "suara-nusa-dev-labs-job"
-  location      = "US"
+resource "google_storage_bucket" "suara-nusa-dev-labs-ml-test-1" {
+  name          = "suara-nusa-dev-labs-ml-test-1"
+  location      = var.region
   force_destroy = true
 
   lifecycle_rule {
@@ -15,7 +15,7 @@ resource "google_storage_bucket" "auto-expire" {
 
 
 resource "google_storage_bucket_iam_member" "cloud_run_access" {
-  bucket = google_storage_bucket.auto-expire.name
+  bucket = google_storage_bucket.suara-nusa-dev-labs-ml-test-1.name
   role   = "roles/storage.objectAdmin"
   member = "serviceAccount:${var.cloud_run_service_account_email}"  # Ganti dengan email service account Cloud Run
 }
--- modules/cloud-storage-job/variables.tf
+++ modules/cloud-storage-job/variables.tf
@@ -2,3 +2,8 @@ variable "cloud_run_service_account_email" {
   description = "The service account that is used by the CloudRun service"
   type        = string
 }
+
+variable "region" {
+  description = "The region where the bucket will be deployed"
+  type        = string
+}
